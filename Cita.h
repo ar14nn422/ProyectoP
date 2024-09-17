3417e5c8c3152549b6f4f801268dfe7838922e17
Tu Nombre <tuemail@example.com>
2024-09-16 19:45:32 -0600

    cita.h

--- Cita.h
+++ Cita.h
@@ -1,16 +1,16 @@
 ﻿#include "ColeccionDueños.h"
 #include "coleccionmascotas.h"
-#include "ColeccionDoctores.h"
+#include "especialidad.h"
 class Cita {
 private:
 	int hora;
 	string dia;
 	bool estado;
-	ColeccionDoctores* doctor;//cambiar por el nombre de la coleccion
+	especialidad* doctor;//cambiar por el nombre de la coleccion
 	ColeccionDueños* duenno;
 	coleccionmascotas* paciente;
 public:
-	Cita(int, string, bool, ColeccionDoctores*, ColeccionDueños*, coleccionmascotas*);
+	Cita(int, string, bool, especialidad*, ColeccionDueños*, coleccionmascotas*);
 	Cita();
 	int getHora();
 	string getDia();
@@ -18,10 +18,10 @@ public:
 	void setDia(string);
 	void setEstado(bool);
 	bool getEstado();
-	ColeccionDoctores* getDoctor();
+	especialidad* getDoctor();
 	ColeccionDueños* getDuenno();
 	coleccionmascotas* getPaciente();
-	void setDoctor(ColeccionDoctores*);
+	void setDoctor(especialidad*);
 	void setDuenno(ColeccionDueños*);
 	void setPaciente(coleccionmascotas*);
 	void reactivarCita();
