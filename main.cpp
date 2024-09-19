3bc5d33586e4d58a4c12c7af8a5a158d428aa4e9
Tu Nombre <tuemail@example.com>
2024-09-18 18:40:46 -0600

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include"menuprincipal.h"
 int main() {
-	
 	menuprincipal c1;
 	c1.menu();
 	cout << "Gracias por utilizar nuestro servicio" << endl;
 	c1.~menuprincipal();
 	cin.get();
 	cin.get();
+	return 0;
 }
